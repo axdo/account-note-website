152d09046878c9e81ce8cae80619d2127f11db3a
cobra <axcaptain@126.com>
2021-06-29 11:01:59 +0800

    deploy

---
+++ precache-manifest.6583d12642d16aef3b29ea3a7a03831d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46848289684d39865f68",
+    "url": "/account-note-website/css/app.9dcc4b80.css"
+  },
+  {
+    "revision": "3aee601ea674fbe20e9e93dd70598f57",
+    "url": "/account-note-website/img/qrcode.3aee601e.png"
+  },
+  {
+    "revision": "531f3398934d07a516c183b6cfef61cd",
+    "url": "/account-note-website/index.html"
+  },
+  {
+    "revision": "46848289684d39865f68",
+    "url": "/account-note-website/js/app.3d7ec1bf.js"
+  },
+  {
+    "revision": "0484c6c67a39f4ef7db0",
+    "url": "/account-note-website/js/chunk-vendors.2e2c85f1.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/account-note-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-note-website/robots.txt"
+  }
+]);
